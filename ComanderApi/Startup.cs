560f1052e111cf402c375b29f025df6afe24ab95
Osama Tab <Osama.Tabib@outlook.com>
2020-07-18 20:24:03 +0300

    adding swagger

--- ComanderApi/Startup.cs
+++ ComanderApi/Startup.cs
@@ -28,9 +28,20 @@ namespace ComanderApi
         {
 
             services.AddControllers().AddNewtonsoftJson(options =>
-    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-);
-            services.AddTransient<IMovieService, MockMovie>();
+             options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+            services.AddSwaggerGen(options =>
+            { 
+                options.SwaggerDoc("v1",
+                new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Swagger Demo API",
+                    Description = "Demo API for showing Swagger",
+                    Version = "v1"
+                });
+            });
+
+          services.AddTransient<IMovieService, MockMovie>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +61,10 @@ namespace ComanderApi
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+            app.UseSwagger();
+            app.UseSwaggerUI(options=>
+            { options.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger Demo API");
             });
         }
     }
